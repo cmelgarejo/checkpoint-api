efa0bf13afff40f73946c305b026d8c935b731d5
Christian Melgarejo <cmelgarejo.dev@gmail.com>
2018-11-04 23:07:58 -0300

    Add comments to  UserController

--- app/Controllers/Http/v1/UserController.js
+++ app/Controllers/Http/v1/UserController.js
@@ -1,8 +1,23 @@
 'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
 const User = use('App/Models/User')
 const JsonApiRB = use('JsonApiRecordBrowser')
 
+/**
+ * Resourceful controller for interacting with users
+ */
 class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async index({ request, params }) {
     //if has permissions, show all users
     return await JsonApiRB.model(User)
@@ -10,6 +25,14 @@ class UserController {
       .paginateOrFetch()
   }
 
+  /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async update({ request, params }) {
     const { id } = params
     const user = await User.findOrFail(id)
@@ -20,18 +43,41 @@ class UserController {
     return user
   }
 
+  /**
+   * Display a single user.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async show({ params }) {
     const { id } = params
     const user = await User.findOrFail(id)
     return user
   }
 
+  /**
+   * Delete a user with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async destroy({ params }) {
     const { id } = params
     const user = await User.findOrFail(id)
     user.delete()
   }
 
+  /**
+   * Generates an API Key.
+   * POST users/apikey
+   *
+   * @param {object} ctx
+   * @param {Response} ctx.response
+   */
   async generateAPIKey({ response, auth }) {
     const user = await auth.getUser()
     const authAPI = await auth.authenticator('api')
@@ -41,6 +87,13 @@ class UserController {
     return token
   }
 
+  /**
+   * Generates an JWT Key.
+   * POST users/token
+   *
+   * @param {object} ctx
+   * @param {Response} ctx.response
+   */
   async generateJWTKey({ response, auth }) {
     const user = await auth.getUser()
     const authAPI = await auth.authenticator('jwt')
@@ -49,6 +102,13 @@ class UserController {
     return token
   }
 
+  /**
+   * Refreshes an JWT Key.
+   * POST users/token
+   *
+   * @param {object} ctx
+   * @param {Response} ctx.response
+   */
   async refreshJWTKey({ response, auth }) {
     const user = await auth.getUser()
     const authAPI = await auth.authenticator('jwt')
