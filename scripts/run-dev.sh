330f97db4d7050372f8eb2905d9fc85219a86cd2
Christian Melgarejo Bresanovich <cmelgarejo.dev@gmail.com>
2019-01-06 10:07:42 -0300

    Fix script  whitespace

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Windows: run this as "sh ./scripts/run-dev.sh"
 echo "Running OS type: $OSTYPE"
-if [ "$OSTYPE" == "msys" ] || [ "$OSTYPE" == "win32"] ; then
+if [ "$OSTYPE" == "msys" ] || [ "$OSTYPE" == "win32" ] ; then
   nodemon server.js
 else
   sudo nodemon server.js
